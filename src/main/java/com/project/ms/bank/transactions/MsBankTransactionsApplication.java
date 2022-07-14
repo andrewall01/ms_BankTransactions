a64c94b7290a77d291b6cfd607804cbb12934e7f
Andres Llontop <andresllontop01@gmail.com>
2022-07-14 18:33:42 -0500

    Se actualiza archivo pom.xml

--- src/main/java/com/project/ms/bank/transactions/MsBankTransactionsApplication.java
+++ src/main/java/com/project/ms/bank/transactions/MsBankTransactionsApplication.java
@@ -2,10 +2,12 @@ package com.project.ms.bank.transactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
 @EnableReactiveMongoAuditing
+@EnableEurekaClient
 public class MsBankTransactionsApplication {
 
 	public static void main(String[] args) {
